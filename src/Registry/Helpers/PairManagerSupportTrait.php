1e0d17f1e5476ce528d024244f5d2b2f15c7d895
Alexandr Shevchenko <ls.shov@gmail.com>
2018-01-23 19:41:03 +0300

    Wtf? did git turned me down with tis files?

---
+++ src/Registry/Helpers/PairManagerSupportTrait.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace Shov\Registry\Helpers;
+use Shov\Registry\Contracts\LoaderInterface;
+use Shov\Registry\Contracts\SaverInterface;
+use Shov\Registry\Exceptions\LockedException;
+
+/**
+ * Help to manage pairs
+ *
+ * Required:
+ * @property $pairs
+ * @property $immutablePairs
+ * @property SaverInterface $saver
+ * @property LoaderInterface $loader
+ */
+trait PairManagerSupportTrait
+{
+    /**
+     * Check if key is an empty string, throw an exception
+     * @param string $key
+     */
+    protected function breakIfEmpty(string $key)
+    {
+        if(empty($key)) {
+            throw new \InvalidArgumentException("Empty key is invalid key!");
+        }
+    }
+
+    /**
+     * Check if the pair with given key is immutable one then throw the exception
+     * @param string $key
+     * @throws LockedException
+     */
+    protected function breakIfLocked(string $key)
+    {
+        if (isset($this->immutablePairs[$key])) {
+            throw new LockedException();
+        }
+    }
+
+    /**
+     * Save the state
+     */
+    protected function saveState()
+    {
+        $this->saver->save($this->pairs);
+    }
+
+    /**
+     * Load the state
+     */
+    protected function loadState()
+    {
+        $this->pairs = $this->loader->load();
+    }
+}
